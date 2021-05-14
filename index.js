385501d84bcb63fe13d373c5c4bddd3451011eda
Reedam Choudhary <70550395+reedamchoudhary@users.noreply.github.com>
2021-05-14 19:08:22 +0530

    portfolio part done

--- index.js
+++ index.js
@@ -1,47 +1,47 @@
-// const hamburger = document.querySelector(".hamburger")
-// console.log("inside js")
-// hamburger.addEventListener("click",()=>{
-//     console.log("inside event listener")
-//     document.querySelector(".navigation-bar").classList.add("visible")
-//     document.querySelector(".navigation-bar").style.transition = "all 2s";
-// })
+// // const hamburger = document.querySelector(".hamburger")
+// // console.log("inside js")
+// // hamburger.addEventListener("click",()=>{
+// //     console.log("inside event listener")
+// //     document.querySelector(".navigation-bar").classList.add("visible")
+// //     document.querySelector(".navigation-bar").style.transition = "all 2s";
+// // })
 
-// const cancel = document.querySelector(".fas")
-// cancel.addEventListener("click",()=>{
-//     console.log("inside event listener2")
-//     document.querySelector(".navigation-bar").classList.remove("visible")
-// })
+// // const cancel = document.querySelector(".fas")
+// // cancel.addEventListener("click",()=>{
+// //     console.log("inside event listener2")
+// //     document.querySelector(".navigation-bar").classList.remove("visible")
+// // })
 
-const button = document.querySelector(".hamburger")
-console.log(button)
-const navBar = document.querySelector(".navigation-bar")
+// const button = document.querySelector(".hamburger")
+// console.log(button)
+// const navBar = document.querySelector(".navigation-bar")
 
-var height = navBar.clientHeight
-var width = navBar.clientWidth
+// var height = navBar.clientHeight
+// var width = navBar.clientWidth
 
-console.log(width+ " " +height)
-navBar.style.height = height+'px'
-navBar.style.width = width+'px'
+// console.log(width+ " " +height)
+// navBar.style.height = height+'px'
+// navBar.style.width = width+'px'
 
-button.addEventListener('click', ()=>{
-    console.log("inside event list")
+// button.addEventListener('click', ()=>{
+//     console.log("inside event list")
     
         
-        navBar.style.height = height+'px';
-        navBar.style.width = width + 'px';
-        navBar.style.padding = '.5em';
-        navBar.style.visibility='visible';
+//         navBar.style.height = height+'px';
+//         navBar.style.width = width + 'px';
+//         navBar.style.padding = '.5em';
+//         navBar.style.visibility='visible';
     
     
         
-})
+// })
 
-document.querySelector("i").addEventListener('click',()=>{
+// document.querySelector("i").addEventListener('click',()=>{
     
-        //navBar.style.opacity = '0';
-        navBar.style.height = '0';
-        navBar.style.width = '0';
-        navBar.style.padding = '0';
-        navBar.style.visibility = 'hidden';
+//         //navBar.style.opacity = '0';
+//         navBar.style.height = '0';
+//         navBar.style.width = '0';
+//         navBar.style.padding = '0';
+//         navBar.style.visibility = 'hidden';
       
-})
+// })
