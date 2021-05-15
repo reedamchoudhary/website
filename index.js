ac1ac5bd65338b1691825f5fc40bbe63ae51a59a
Reedam Choudhary <70550395+reedamchoudhary@users.noreply.github.com>
2021-05-15 09:09:59 +0530

    added animation to head

--- index.js
+++ index.js
@@ -1,47 +1,17 @@
-// // const hamburger = document.querySelector(".hamburger")
-// // console.log("inside js")
-// // hamburger.addEventListener("click",()=>{
-// //     console.log("inside event listener")
-// //     document.querySelector(".navigation-bar").classList.add("visible")
-// //     document.querySelector(".navigation-bar").style.transition = "all 2s";
-// // })
+window.onload= ()=>{
+    setTimeout(()=>{
+        document.querySelector(".title hr").style.width = "10%"},
+        3000
+    )
+    setTimeout(()=>{
+        document.querySelectorAll(".title .head h1")[0].style.fontSize = "10rem"
+        document.querySelectorAll(".title .head h1")[1].style.fontSize = "10rem"
+    },
+        1000
+    )
 
-// // const cancel = document.querySelector(".fas")
-// // cancel.addEventListener("click",()=>{
-// //     console.log("inside event listener2")
-// //     document.querySelector(".navigation-bar").classList.remove("visible")
-// // })
-
-// const button = document.querySelector(".hamburger")
-// console.log(button)
-// const navBar = document.querySelector(".navigation-bar")
-
-// var height = navBar.clientHeight
-// var width = navBar.clientWidth
-
-// console.log(width+ " " +height)
-// navBar.style.height = height+'px'
-// navBar.style.width = width+'px'
-
-// button.addEventListener('click', ()=>{
-//     console.log("inside event list")
-    
-        
-//         navBar.style.height = height+'px';
-//         navBar.style.width = width + 'px';
-//         navBar.style.padding = '.5em';
-//         navBar.style.visibility='visible';
-    
-    
-        
-// })
-
-// document.querySelector("i").addEventListener('click',()=>{
-    
-//         //navBar.style.opacity = '0';
-//         navBar.style.height = '0';
-//         navBar.style.width = '0';
-//         navBar.style.padding = '0';
-//         navBar.style.visibility = 'hidden';
-      
-// })
+    setTimeout(()=>{
+        document.querySelector(".title h2").style.transform = "translateY(0px)"},
+        4000
+    )
+}
